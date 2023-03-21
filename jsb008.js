3dd18cd6afb0607ffd5830803c7d844dbc99cff3
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-03-21 15:30:20 -0500

    changes for JS taking again from the basics

--- jsb008.js
+++ jsb008.js
@@ -2,7 +2,12 @@
 // Not possible hosting on classes
 
 class Persona {
-  static contadorObjetosPersona = 0;
+  //se asocia solo con la clase
+  static contadorObjetosPersona = 0; //atribute for the class
+
+  //se asocia con los objetos a crear
+  email = "email@default.com"; // atributte for the objects
+
   constructor(nombre, apellido) {
     this._nombre = nombre;
     this._apellido = apellido;
@@ -63,6 +68,10 @@ class Empleado extends Persona {
 
 let persona1 = new Persona("john", "Willson");
 let empleado1 = new Empleado("Yisus", "Fisherman", "Sales");
+
+console.log(persona1.email);
+console.log(empleado1.email);
+
 //is not posible from the object persona1.saludar();
 Persona.saludar();
 Persona.saludar2(persona1);
@@ -72,3 +81,78 @@ console.log(Empleado.contadorObjetosPersona);
 
 Empleado.saludar();
 Empleado.saludar2(empleado1);
+
+/** STATIC ANOTHER EXAMPLE*/
+console.log("/** STATIC ANOTHER EXAMPLE*/");
+class PersonaNew {
+  //se asocia solo con la clase
+  static contadorObjetosPersonas = 0; //atribute for the class
+
+  static get MAX_OBJ() {
+    return 5;
+  }
+
+  constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+    //access static use the class
+    if (PersonaNew.contadorObjetosPersonas < PersonaNew.MAX_OBJ) {
+      this.idPersona = ++PersonaNew.contadorObjetosPersonas;
+    } else {
+      console.log("Superado el maximo de objetos permitidos");
+    }
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  get apellido() {
+    return this._apellido;
+  }
+
+  set apellido(apellido) {
+    this._apellido = apellido;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+
+  nombreCompleto() {
+    return "ID: " + this.idPersona + " " + this._nombre + " " + this._apellido;
+  }
+  //overwrite toString
+  toString() {
+    //poliformismo multiples formas en tiempo de ejecucion el metodo
+    //que se ejecuta depende si es una referencia de tipo padre o hijo
+    return this.nombreCompleto();
+  }
+
+  //static method
+  static saludar() {
+    console.log("Hello Persona from method static");
+  }
+
+  static saludar2(persona) {
+    console.log(persona.nombre + " " + persona.apellido);
+  }
+}
+
+let persona2 = new PersonaNew("Juanes", "Willson");
+console.log(persona2.nombreCompleto());
+console.log(persona2.toString());
+
+let persona3 = new PersonaNew("Charlie2", "Brown");
+console.log(persona3.toString());
+
+console.log(PersonaNew.MAX_OBJ);
+
+let persona4 = new PersonaNew("Charlie3", "Brown");
+console.log(persona4.toString());
+
+let persona5 = new PersonaNew("Charlie4", "Brown");
+console.log(persona5.toString());
+
+let persona6 = new PersonaNew("Charlie45", "Brown");
+console.log(persona6.toString());
+
+let persona7 = new PersonaNew("Charlie6", "Brown");
+console.log(persona7.toString());
