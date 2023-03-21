91a3d4586d071a3ce098d0a8218c3ec3896c8a10
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-03-21 03:58:04 -0500

    changes for JS taking again from the basics

--- jsb007.js
+++ jsb007.js
@@ -19,11 +19,21 @@ class Persona {
   set nombre(nombre) {
     this._nombre = nombre;
   }
+
+  nombreCompleto() {
+    return this._nombre + " " + this._apellido;
+  }
+  //overwrite toString
+  toString(){
+    //poliformismo multiples formas en tiempo de ejecucion el metodo
+    //que se ejecuta depende si es una referencia de tipo padre o hijo
+    return this.nombreCompleto();
+  }
 }
 
 class Empleado extends Persona {
   constructor(nombre, apellido, departamento) {
-    super(nombre, apellido) //constructor from the super class
+    super(nombre, apellido); //constructor from the super class
     this._departamento = departamento;
   }
   get departamento() {
@@ -32,14 +42,22 @@ class Empleado extends Persona {
   set departamento(departamento) {
     this._departamento = departamento;
   }
+  //overwrite
+  nombreCompleto(){
+    return super.nombreCompleto() + " " + this._departamento;
+  }
+
 }
 
 let persona1 = new Persona("John", "Carpenter");
-console.log(persona1)
+console.log(persona1);
+console.log(persona1.toString());
 
 let empleado1 = new Empleado("Yisus", "Fisherman", "Sales");
-console.log(empleado1);
-console.log(empleado1.nombre);
+
+console.log(empleado1.nombreCompleto());
+
+console.log(empleado1.toString());
 
 //calling the method get
 console.log(persona1.nombre);
