2bd4ccbedd8c308e33d0828c01086ce2bd440a46
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-03-21 03:38:56 -0500

    changes for JS taking again from the basics

--- jsb007.js
+++ jsb007.js
@@ -1,4 +1,6 @@
 /*****clases */
+// Not possible hosting on classes
+
 class Persona {
   constructor(nombre, apellido) {
     this._nombre = nombre;
@@ -19,7 +21,26 @@ class Persona {
   }
 }
 
+class Empleado extends Persona {
+  constructor(nombre, apellido, departamento) {
+    super(nombre, apellido) //constructor from the super class
+    this._departamento = departamento;
+  }
+  get departamento() {
+    return this._departamento;
+  }
+  set departamento(departamento) {
+    this._departamento = departamento;
+  }
+}
+
 let persona1 = new Persona("John", "Carpenter");
+console.log(persona1)
+
+let empleado1 = new Empleado("Yisus", "Fisherman", "Sales");
+console.log(empleado1);
+console.log(empleado1.nombre);
+
 //calling the method get
 console.log(persona1.nombre);
 console.log(persona1.apellido);
