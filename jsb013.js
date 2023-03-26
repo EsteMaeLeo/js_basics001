25fb78b9dfbc1bcc567fb457c1c5c5c680cae0ff
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-03-25 23:48:02 -0500

    changes for this back to basics

---
+++ jsb013.js
@@ -0,0 +1,12 @@
+//Arrow functions
+let miFucion = function () {
+  console.log("Mensaje desde funcion");
+};
+
+let miFuncionFlecha = () => {
+  console.log("Mensaje desde funcion flecha");
+};
+
+miFucion();
+
+miFuncionFlecha();
