1c421b77c03ec0f9b8ee3c8f69daa196528ff0ce
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-03-25 23:56:40 -0500

    changes for this back to basics

--- jsb013.js
+++ jsb013.js
@@ -3,10 +3,23 @@ const miFucion = function () {
   console.log("Mensaje desde funcion");
 };
 
-const  miFuncionFlecha = () => {
+const miFuncionFlecha = () => {
   console.log("Mensaje desde funcion flecha");
 };
+
+const miFuncionFlecha2 = () => console.log("Mensaje desde funcion flecha #2");
+
+const saludar = () => "Saludos desde function flecha";
+
+const regresaObjeto = () => ({ nombre: "Juan", apellido: "Perez" });
+
+const funcionParametros = (mensaje) => console.log(mensaje)
 
 miFucion();
 
 miFuncionFlecha();
+miFuncionFlecha2();
+
+console.log(saludar());
+console.log(regresaObjeto());
+funcionParametros("saludo con paramentros");
