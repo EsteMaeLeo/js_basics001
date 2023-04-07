9794d87b960e0100fb4414a79bf1da1ebfdd257c
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-04-07 18:21:14 -0500

    changes

--- basic002/objects.js
+++ basic002/objects.js
@@ -8,4 +8,45 @@ const product = {
   available: true,
 };
 
+//access each
+console.log(product.name);
+console.log(product.price);
+console.log(product["name"]);
+
+//add new propeties
 console.log(product);
+product.image = "image.jpg";
+console.log(product);
+
+delete product.available;
+console.log(product);
+
+console.log("destruction objects");
+//const name = product.name;
+//console.log(name);
+
+const { name, price } = product;
+
+console.log(name);
+console.log(price);
+
+//object inside other object
+
+const product_inside = {
+  name: "Keyboard",
+  price: 15,
+  available: true,
+  information: {
+    measure: { weight: "1kg", measure: "1m" },
+    countryFrom: {
+      country: "China",
+    },
+  },
+};
+console.log(product_inside);
+console.log(product_inside.information);
+console.log(product_inside.information.measure);
+console.log(product_inside.information.countryFrom.country);
+
+const { measure, countryFrom } = product_inside.information;
+console.log(measure);
