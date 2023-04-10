f04971e9775c10e97fa7f5430624eb55f56bac23
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-04-09 21:29:41 -0500

    changes

---
+++ basic002/strings.js
@@ -0,0 +1,18 @@
+const producto = "Monitor HP 20";
+const producto2 = String("Monitor HP 20");
+const producto3 = new String("Monitor HP 20");
+
+console.log("Strings");
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
+
+//lenght
+
+console.log("Lenght: ", producto.length);
+
+//index of find 
+console.log("Index of: ", producto.indexOf("HP"));
+
+//includes return boolean
+console.log("Includes: ", producto.includes("HP"));
