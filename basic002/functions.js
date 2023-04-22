5d229b8e90425c7edabecd9986a920424bd93275
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-04-22 02:59:22 -0500

    new changes

---
+++ basic002/functions.js
@@ -0,0 +1,33 @@
+//function declarations able to call function before the definition
+
+function add(number1, number2) {
+  let result = number1 + number2;
+  console.log(`Result: ${result}`);
+}
+
+add(2, 2);
+
+//function expression you can call function before the definition
+
+const add2 = function (number1, number2) {
+  let result = number1 + number2;
+  console.log(`Result: ${result}`);
+};
+
+add2(2, 5);
+
+//difference method and function... method: name variable .nameMethod
+const numero1 = 20;
+const numero2 = "20";
+
+console.log(parseInt(numero2)); //function parseInt
+console.log(numero1.toString());
+
+//Parameters in function and default parameters
+function saidHello(name = "Unknown", lastname = "") {
+  console.log(`Hello ${name} ${lastname}`);
+}
+
+saidHello("John", "Connor");
+saidHello("Glen");
+saidHello();
