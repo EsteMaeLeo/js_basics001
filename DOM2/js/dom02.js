4a00040f9ea9fd537478cc4e63008bb7c0d9cd43
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-04-28 02:13:20 -0500

    new changes

---
+++ DOM2/js/dom02.js
@@ -0,0 +1,8 @@
+const header = document.getElementsByClassName("header");
+console.log(header);
+
+const hero = document.getElementsByClassName("hero");
+console.log(hero)
+
+const contenedor = document.getElementsByClassName("contenedor");
+console.log(contenedor)
