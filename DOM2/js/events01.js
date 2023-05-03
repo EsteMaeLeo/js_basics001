8dbc58ae1346ee133c9ce294f775688d0d02ddcb
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-03 02:07:29 -0500

    changes for dom element using js

--- DOM2/js/events01.js
+++ DOM2/js/events01.js
@@ -1,9 +1,40 @@
-
 console.log(1);
 //after all html is loaded  waits the html is full loaded
-document.addEventListener('DOMContentLoaded', () =>{
-    console.log('Document Ready')
-    console.log(2);
-})
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Document Ready");
+  console.log(2);
+});
 
 console.log(3);
+
+//events mouse
+const nav = document.querySelector(".navegacion");
+nav.addEventListener("dblclick", () => {
+    console.log("mouse dblclick");
+    nav.style.backgroundColor = "back";
+  });
+
+  nav.addEventListener("click", () => {
+    console.log("click mouse");
+  });
+  
+  
+  nav.addEventListener("mouseenter", () => {
+    console.log("mouse enter");
+    nav.style.backgroundColor = "white";
+  });
+  
+  nav.addEventListener("mouseout", () => {
+    console.log("mouse out");
+    nav.style.backgroundColor = "transparent";
+  });
+  
+  nav.addEventListener("mousedown", () => {
+    console.log("mouse down");
+    nav.style.backgroundColor = "yellow";
+  });
+  
+  nav.addEventListener("mouseup", () => {
+    console.log("mouse up");
+    nav.style.backgroundColor = "green";
+  });
