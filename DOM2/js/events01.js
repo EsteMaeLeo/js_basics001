c5fa5b7779c8d4cb171ec0df37b741a6e2d4174a
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-03 01:51:03 -0500

    changes for dom element using js

---
+++ DOM2/js/events01.js
@@ -0,0 +1,9 @@
+
+console.log(1);
+//after all html is loaded  waits the html is full loaded
+document.addEventListener('DOMContentLoaded', () =>{
+    console.log('Document Ready')
+    console.log(2);
+})
+
+console.log(3);
