67d6d70f8c6fc66d53d97f74d59a707f013ecfce
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-05 18:07:39 -0500

    new changes

--- projects/cart/js/app.js
+++ projects/cart/js/app.js
@@ -12,6 +12,23 @@ function loadEventListeners() {
 function addCourse(e) {
   e.preventDefault();
   if (e.target.classList.contains("agregar-carrito")) {
-    console.log(e.target);
+
+    const courseRead = e.target.parentElement.parentElement;
+    readCourse(courseRead);
   }
+}
+
+//read data of HTML which we did the click
+function readCourse(course){
+    console.log(course)
+    const infoCourse ={
+        id : course.querySelector('a').getAttribute('data-id'),
+        img : course.querySelector('img').src,
+        title : course.querySelector('h4').textContent,
+        price : course.querySelector('.precio span').textContent,
+        cantidad : 1
+        
+    }
+
+    console.log(infoCourse);
 }
