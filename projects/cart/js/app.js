5d739dc610d7d2b1a2b5fd6a2ca94bbd4fc3030f
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-05 22:54:58 -0500

    new changes

--- projects/cart/js/app.js
+++ projects/cart/js/app.js
@@ -2,6 +2,7 @@ const cart = document.querySelector("#carrito");
 const containerCart = document.querySelector("#lista-carrito tbody");
 const btEmptyCart = document.querySelector("#vacias-carrito");
 const listCourses = document.querySelector("#lista-cursos");
+let shoppingCart = [];
 
 loadEventListeners();
 
@@ -12,23 +13,46 @@ function loadEventListeners() {
 function addCourse(e) {
   e.preventDefault();
   if (e.target.classList.contains("agregar-carrito")) {
-
     const courseRead = e.target.parentElement.parentElement;
     readCourse(courseRead);
   }
 }
 
 //read data of HTML which we did the click
-function readCourse(course){
-    console.log(course)
-    const infoCourse ={
-        id : course.querySelector('a').getAttribute('data-id'),
-        img : course.querySelector('img').src,
-        title : course.querySelector('h4').textContent,
-        price : course.querySelector('.precio span').textContent,
-        cantidad : 1
-        
-    }
+function readCourse(course) {
+  console.log(course);
+  const infoCourse = {
+    id: course.querySelector("a").getAttribute("data-id"),
+    img: course.querySelector("img").src,
+    title: course.querySelector("h4").textContent,
+    price: course.querySelector(".precio span").textContent,
+    cantidad: 1,
+  };
 
-    console.log(infoCourse);
+  console.log(infoCourse);
+  //adding elments to the shoppping cart
+  shoppingCart = [...shoppingCart, infoCourse];
+  console.log(shoppingCart);
+  showCartHtml();
+}
+
+function showCartHtml() {
+  //clean html
+  deleteHtml();
+  //loop the html
+  shoppingCart.forEach((course) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>
+        ${course.title}
+    </td>
+    `;
+    //add HTML into tbody
+    containerCart.appendChild(row);
+  });
+}
+
+//eliminate the course tbody
+function deleteHtml() {
+  containerCart.innerHTML = "";
 }
