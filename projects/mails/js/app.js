89d651b0206d6f887cd144a8870f9503025fcc69
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-07 22:53:37 -0500

    new changes

---
+++ projects/mails/js/app.js
@@ -0,0 +1,7 @@
+//all html loaded
+document.addEventListener('DOMContentLoaded', function(){
+    const inputEmail = document.querySelector('#email');
+    const inputSubject = document.querySelector('#asunto');
+    const inputMsg = document.querySelector('#mensaje')
+    console.log(inputMsg)
+})
