e1d1bad1883a5813ec1eaff73c987b2ff7a9e0ba
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-07 23:06:23 -0500

    new changes

--- projects/mails/js/app.js
+++ projects/mails/js/app.js
@@ -1,7 +1,15 @@
 //all html loaded
-document.addEventListener('DOMContentLoaded', function(){
-    const inputEmail = document.querySelector('#email');
-    const inputSubject = document.querySelector('#asunto');
-    const inputMsg = document.querySelector('#mensaje')
-    console.log(inputMsg)
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const inputEmail = document.querySelector("#email");
+  const inputSubject = document.querySelector("#asunto");
+  const inputMsg = document.querySelector("#mensaje");
+
+  //trigger in the field but after user click another
+  inputEmail.addEventListener("blur", function (e) {
+    //access the value or the text in the input
+    console.log(e.target.value);
+  });
+
+
+  console.log(inputMsg);
+});
