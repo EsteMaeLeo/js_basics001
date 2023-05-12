c85bf04ee0892f698ce945885b01066510d39c58
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-11 22:37:12 -0500

    new changes

--- projects/mails/js/app.js
+++ projects/mails/js/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputSubject = document.querySelector("#asunto");
   const inputMsg = document.querySelector("#mensaje");
+  const form = document.querySelector("#formulario");
 
   //trigger in the field but after user click another
   inputEmail.addEventListener("blur", validate);
@@ -13,6 +14,25 @@ document.addEventListener("DOMContentLoaded", function () {
   inputMsg.addEventListener("blur", validate);
 
   function validate(e) {
-    console.log(e.target.value);
+    console.log(e.target.parentElement);
+    //e.target.parentElement.nextElementSibling
+    console.log(e.target.id);
+    if (e.target.value.trim() === "") {
+      showAlert(`Field ${e.target.id} is obligatory`, e.target.parentElement);
+    } else {
+    }
+  }
+
+  function showAlert(msg, reference) {
+    //validate if there is allready alert creates use reference the parent only on the div of the field
+    const alert = reference.querySelector(".bg-red-600");
+    if(alert){
+      alert.remove();
+    }
+    //generate HTML
+    const error = document.createElement("P");
+    error.textContent = msg;
+    error.classList.add("bg-red-600", "text-white", "text-center", "p-2");
+    reference.appendChild(error);
   }
 });
