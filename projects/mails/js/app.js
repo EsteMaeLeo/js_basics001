ebe9883ba452f59b7593d2adf5773b8024ae6e58
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-12 17:54:56 -0500

    new changes

--- projects/mails/js/app.js
+++ projects/mails/js/app.js
@@ -1,17 +1,28 @@
 //all html loaded
 document.addEventListener("DOMContentLoaded", function () {
+  const email = {
+    email: "",
+    asunto: "",
+    mensaje: "",
+  };
+
   const inputEmail = document.querySelector("#email");
   const inputSubject = document.querySelector("#asunto");
   const inputMsg = document.querySelector("#mensaje");
   const form = document.querySelector("#formulario");
+  const btnSubmit = document.querySelector('#formulario button[type="submit"]');
 
   //trigger in the field but after user click another
-  inputEmail.addEventListener("blur", validate);
+  //inputEmail.addEventListener("blur", validate);
 
   //trigger in the field but after user click another
-  inputSubject.addEventListener("blur", validate);
+  //inputSubject.addEventListener("blur", validate);
   //trigger in the field but after user click another
-  inputMsg.addEventListener("blur", validate);
+  //inputMsg.addEventListener("blur", validate);
+  //realtime
+  inputEmail.addEventListener("input", validate);
+  inputSubject.addEventListener("input", validate);
+  inputMsg.addEventListener("input", validate);
 
   function validate(e) {
     console.log(e.target.parentElement);
@@ -19,14 +30,26 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(e.target.id);
     if (e.target.value.trim() === "") {
       showAlert(`Field ${e.target.id} is obligatory`, e.target.parentElement);
+      email[e.target.name] = "";
+      checkEmail();
       return;
     }
 
     if (e.target.id === "email" && !valEmail(e.target.value)) {
-      showAlert(`Email: ${e.target.value} is not valid`, e.target.parentElement);
+      showAlert(
+        `Email: ${e.target.value} is not valid`,
+        e.target.parentElement
+      );
+      email[e.target.name] = "";
+      checkEmail();
       return;
     }
     cleanAlert(e.target.parentElement);
+
+    //asign the values
+    email[e.target.name] = e.target.value.trim().toLowerCase();
+    console.log(email);
+    checkEmail();
   }
 
   function showAlert(msg, reference) {
@@ -37,21 +60,33 @@ document.addEventListener("DOMContentLoaded", function () {
     error.classList.add("bg-red-600", "text-white", "text-center", "p-2");
     reference.appendChild(error);
   }
-});
 
-//move the reference to know which alert
-function cleanAlert(reference) {
-  //validate if there is allready alert creates use reference the parent only on the div of the field
-  const alert = reference.querySelector(".bg-red-600");
-  if (alert) {
-    alert.remove();
+  //move the reference to know which alert
+  function cleanAlert(reference) {
+    //validate if there is allready alert creates use reference the parent only on the div of the field
+    const alert = reference.querySelector(".bg-red-600");
+    if (alert) {
+      alert.remove();
+    }
   }
-}
 
-function valEmail(email) {
-  const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  function valEmail(email) {
+    const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-  const result = regex.test(email);
-  console.log(result);
-  return result;
-}
+    const result = regex.test(email);
+    console.log(result);
+    return result;
+  }
+
+  function checkEmail() {
+    //return array with . includes if one contain empty value return true
+    console.log(Object.values(email).includes(""));
+    if (Object.values(email).includes("")) {
+      btnSubmit.classList.add("opacity-50");
+      btnSubmit.disabled = true;
+      return;
+    }
+    btnSubmit.classList.remove("opacity-50");
+    btnSubmit.disabled = false;
+  }
+});
