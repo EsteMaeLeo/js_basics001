d687fda5b83cbc89f75538ef9bd496285e1dcb74
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-05-31 01:46:33 -0500

    new

--- projects/prototypes/app.js
+++ projects/prototypes/app.js
@@ -23,13 +23,47 @@ function formatCompany(company) {
   return `Company: ${name} has a balance of ${balance} and belog to category: ${category}`;
 }
 
-
 console.log(formatClient(john));
 
-function Company(name, balance, category){
+function Company(name, balance, category) {
   this.name = name;
   this.balance = balance;
-  this.category = category
+  this.category = category;
 }
-const CompanyJohn = new Company('ERP John', 8900, 'Android, IOS, ERP Systems');
+const CompanyJohn = new Company("ERP John", 8900, "Android, IOS, ERP Systems");
 console.log(formatCompany(CompanyJohn));
+
+//Prototypes
+console.log("PROTOTYPES")
+function ClientProto(name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
+
+//method exclusive for the object
+ClientProto.prototype.typeClient = function () {
+  let type;
+  if (this.balance > 1000) {
+    type = "Gold";
+  } else if (this.balance > 5000) {
+    type = "Platinum";
+  } else {
+    type = "Normal";
+  }
+  return type;
+};
+
+ClientProto.prototype.NameBalance = function(){
+  return `Client: ${this.name} has a balance of ${this.balance}, type Client : ${this.typeClient()}`;
+}
+
+ClientProto.prototype.withdraw = function(withdrawValue){
+  this.balance -= withdrawValue
+}
+
+const william = new ClientProto("William", 15000);
+console.log(william.typeClient());
+console.log(william.NameBalance());
+william.withdraw(250);
+console.log(william.NameBalance());
+console.log(william);
