592f2428cb532d275305f59c56e0c77359b994db
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-06-03 22:21:52 -0500

    new

--- projects/POO/js/app.js
+++ projects/POO/js/app.js
@@ -4,11 +4,20 @@ class Client {
     this.name = name;
     this.balance = balance;
   }
+  showInfo() {
+    return `Client: ${this.name} Blance: ${this.balance}`;
+  }
+
+  static atmWelcome() {
+    return `Welcome to the ATM`;
+  }
 }
 
 //instance
 const will = new Client("William", 5000);
 console.log(will);
+console.log(Client.atmWelcome())
+console.log(will.showInfo())
 
 //class expresion
 const Client2 = class {
@@ -16,7 +25,11 @@ const Client2 = class {
     this.name = name;
     this.balance = balance;
   }
+  showInfo() {
+    return `Client: ${this.name} Blance: ${this.balance}`;
+  }
 };
 
-const will2 = new Client2("William", 5000);
+const will2 = new Client2("Will", 5000);
 console.log(will2);
+console.log(will2.showInfo());
