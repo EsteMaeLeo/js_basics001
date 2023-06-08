c7afaddc2b378de99571fb34c528e663f15ab2c7
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-06-08 01:47:15 -0500

    new

--- projects/expense/js/app.js
+++ projects/expense/js/app.js
@@ -6,11 +6,40 @@ function eventListeners() {
   document.addEventListener("DOMContentLoaded", askBudget);
 }
 
+class Budget {
+  constructor(budget) {
+    this.budget = budget;
+    this.remaining = budget;
+    this.expense = [];
+  }
+}
+
+class userInterface {
+    insertBudget(globalBudget){
+        const {budget, remaining} = globalBudget;
+        document.querySelector('#total').textContent = budget;
+        document.querySelector('#restante').textContent = remaining;
+    }
+}
+
+const ui = new userInterface();
+let budget;
+
 function askBudget() {
   const budgetUser = prompt("Add your budget");
   console.log(budgetUser);
   const numberBudget = Number(budgetUser);
-  if (budgetUser === "" || budgetUser === null || isNaN(budgetUser) || budgetUser <=0 ) {
+  if (
+    budgetUser === "" ||
+    budgetUser === null ||
+    isNaN(budgetUser) ||
+    budgetUser <= 0
+  ) {
     window.location.reload();
   }
+
+  budget = new Budget(numberBudget);
+  console.log(budget);
+  ui.insertBudget(budget);
+
 }
