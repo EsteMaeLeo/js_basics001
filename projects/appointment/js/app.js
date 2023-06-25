b8b2ed22954b263dea05d3b32cbd3683716243d5
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-06-25 05:42:04 -0500

    changes

--- projects/appointment/js/app.js
+++ projects/appointment/js/app.js
@@ -5,15 +5,32 @@ const dateInput = document.querySelector("#fecha");
 const timeInput = document.querySelector("#hora");
 const sympInputs = document.querySelector("#sintomas");
 
-const form = document.querySelector('#nueva-cita')
-const appoitments = document.querySelector('#citas');
+const form = document.querySelector("#nueva-cita");
+const appoitments = document.querySelector("#citas");
+
+const citaObj = {
+  pet: "",
+  owner: "",
+  phone: "",
+  date: "",
+  hour: "",
+  symptoms: "",
+};
 
 eventListener();
 
-function eventListener(){
-    petInput.addEventListener('input', dataAppoitment);
+function eventListener() {
+  petInput.addEventListener("input", dataAppoitment);
+  ownerInput.addEventListener("input", dataAppoitment);
+  phoneInput.addEventListener("input", dataAppoitment);
+  dateInput.addEventListener("input", dataAppoitment);
+  timeInput.addEventListener("input", dataAppoitment);
+  sympInputs.addEventListener("input", dataAppoitment);
 }
 
-function dataAppoitment(e){
-    console.log(e.target.value);
+function dataAppoitment(e) {
+  console.log(e.target.name);
+  //should be same name in HTML name and object
+  citaObj[e.target.name] = e.target.value;
+  console.log(citaObj);
 }
