5e7281c9d36947bb57f80923de934d6ab7ebbcea
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-06-29 23:21:04 -0500

    changes

--- projects/appointment/js/app.js
+++ projects/appointment/js/app.js
@@ -27,6 +27,12 @@ class Appoitments {
     this.appoitments = [...this.appoitments, appoitment];
     console.log(this.appoitments);
   }
+
+  deleteAppoitment(id) {
+    this.appoitments = this.appoitments.filter(
+      (appotiment) => appotiment.id !== id
+    );
+  }
 }
 
 class UI {
@@ -72,15 +78,23 @@ class UI {
 
       const dateParagraph = document.createElement("p");
       dateParagraph.classList.add("card-title", "font-weight-bolder");
-      dateParagraph.innerHTML = ` <span class="font-weight-bolder">Phone: </span> ${date}`;
+      dateParagraph.innerHTML = ` <span class="font-weight-bolder">Date: </span> ${date}`;
 
       const hourParagraph = document.createElement("p");
       hourParagraph.classList.add("card-title", "font-weight-bolder");
-      hourParagraph.innerHTML = ` <span class="font-weight-bolder">Phone: </span> ${hour}`;
+      hourParagraph.innerHTML = ` <span class="font-weight-bolder">Hour: </span> ${hour}`;
 
       const symptomsParagraph = document.createElement("p");
       symptomsParagraph.classList.add("card-title", "font-weight-bolder");
-      symptomsParagraph.innerHTML = ` <span class="font-weight-bolder">Phone: </span> ${symptoms}`;
+      symptomsParagraph.innerHTML = ` <span class="font-weight-bolder">Symptoms: </span> ${symptoms}`;
+
+      //button delete
+      const btnDelete = document.createElement("button");
+      btnDelete.classList.add("btn", "btn-danger", "mr-2");
+      btnDelete.innerHTML =
+        'Delete <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>';
+
+      btnDelete.onclick = () => deleteAppoitment(id);
 
       divAppoitment.appendChild(petParagraph);
       divAppoitment.appendChild(ownerParagraph);
@@ -88,6 +102,7 @@ class UI {
       divAppoitment.appendChild(dateParagraph);
       divAppoitment.appendChild(hourParagraph);
       divAppoitment.appendChild(symptomsParagraph);
+      divAppoitment.appendChild(btnDelete);
 
       appoitmentsContainer.appendChild(divAppoitment);
     });
@@ -97,7 +112,6 @@ class UI {
       appoitmentsContainer.removeChild(appoitmentsContainer.firstChild);
     }
   }
-
 }
 
 const ui = new UI();
@@ -154,4 +168,13 @@ function resetObj() {
   appoitmentObj.date = "";
   appoitmentObj.hour = "";
   appoitmentObj.symptoms = "";
+}
+
+function deleteAppoitment(id) {
+  //delete
+  appoitmentsManagement.deleteAppoitment(id);
+  //show message
+  ui.printAlert("Appoitment deleted");
+  //refresh
+  ui.printAppoitment(appoitmentsManagement);
 }
