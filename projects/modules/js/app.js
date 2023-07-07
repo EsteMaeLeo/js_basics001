27fb9984e5329f20a2583839b978137cb04a14e6
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-07-07 05:39:21 -0500

    changes

---
+++ projects/modules/js/app.js
@@ -0,0 +1,7 @@
+import { nameClient, budget, showInfo, hasBudget } from "../js/client.js";
+
+console.log(nameClient);
+console.log(budget);
+
+console.log(showInfo(nameClient, budget));
+hasBudget(budget);
---
+++ projects/modules/js/client.js
@@ -0,0 +1,14 @@
+export const nameClient = "John Williams";
+export const budget = 3000;
+
+export function showInfo(nameClient, budget) {
+  return `Client: ${nameClient} - Budget ${budget}`;
+}
+
+export function hasBudget(budget) {
+  if (budget > 0) {
+    console.log("Has budget of: ", budget);
+  } else {
+    console.log("Dont have Budget assigned");
+  }
+}
