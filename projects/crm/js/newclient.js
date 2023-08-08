d7f0bf609ac7b085fa75469d497a338d1486ab98
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-08 01:01:08 -0500

    new changes

--- projects/crm/js/newclient.js
+++ projects/crm/js/newclient.js
@@ -1,20 +1,76 @@
 //set local functions and variables
-(function(){
-    let DB;
+(function () {
+  let DB;
+  const form = document.querySelector("#formulario");
 
-    document.addEventListener('DOMContentLoaded', ()=>{
-        conectDB();
-    });
+  document.addEventListener("DOMContentLoaded", () => {
+    conectDB();
 
-    function conectDB(){
-        const openDB = window.indexedDB.open('CRM', 1);
+    form.addEventListener("submit", validClient);
+  });
 
-        openDB.onerror = function(){
-            console.log('Error OPEN DB');
-        }
+  function conectDB() {
+    const openDB = window.indexedDB.open("CRM", 1);
 
-        openDB.onsuccess = function(){
-            DB = openDB.result;
-        }
+    openDB.onerror = function () {
+      console.log("Error OPEN DB");
+    };
+
+    openDB.onsuccess = function () {
+      DB = openDB.result;
+    };
+  }
+
+  function validClient(e) {
+    e.preventDefault();
+
+    //read all inputs
+    const name = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#telefono").value;
+    const company = document.querySelector("#empresa").value;
+    console.log(name.value);
+    if (name === "" || email === "" || phone === "" || company === "") {
+      printAlert("Fields are required", "error");
+
+      return;
     }
+  }
+
+  function printAlert(msg, type) {
+    const alert = document.querySelector(".alert");
+
+    if (!alert) {
+      const divAlert = document.createElement("div");
+      divAlert.classList.add(
+        "px-4",
+        "py-3",
+        "rounded",
+        "max-w-lg",
+        "mx-auto",
+        "mt-6",
+        "text-center",
+        "border",
+        "alert"
+      );
+
+      if (type === "error") {
+        divAlert.classList.add("bg-red-100", "border-red-400", "text-red-700");
+      } else {
+        divAlert.classList.add(
+          "bg-green-100",
+          "border-green-400",
+          "text-green-700"
+        );
+      }
+
+      divAlert.textContent = msg;
+
+      form.appendChild(divAlert);
+
+      setTimeout(() => {
+        divAlert.remove();
+      }, 3000);
+    }
+  }
 })();
