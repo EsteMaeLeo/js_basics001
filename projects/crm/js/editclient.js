656b86ab98b674b0aeb12bb1e1b9e656b8b3470f
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-15 23:27:12 -0500

    new changes

--- projects/crm/js/editclient.js
+++ projects/crm/js/editclient.js
@@ -1,9 +1,16 @@
 (function () {
-    document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("DOMContentLoaded", () => {
     //check the id from URL
     const parameterURL = new URLSearchParams(window.location.search);
 
     const idClient = parameterURL.get("id");
     console.log(idClient);
+    if (idClient) {
+      getClient(idClient);
+    }
   });
+
+  function getClient(id) {
+    console.log(id);
+  }
 })();
