768050dc76d9f0777006cb5b1e176e7d05dbff07
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-17 23:29:12 -0500

    new changes

--- projects/crm/js/editclient.js
+++ projects/crm/js/editclient.js
@@ -1,16 +1,39 @@
 (function () {
+  let DB;
+
   document.addEventListener("DOMContentLoaded", () => {
+
+    connectDB();
     //check the id from URL
     const parameterURL = new URLSearchParams(window.location.search);
 
     const idClient = parameterURL.get("id");
     console.log(idClient);
     if (idClient) {
-      getClient(idClient);
+      setTimeout(()=>{
+        getClient(idClient);
+      }, 1000)
+      
     }
   });
 
   function getClient(id) {
     console.log(id);
+    const transaction = DB.transaction(['crm'], 'readwrite');
+
+    const objectStore = transaction.objectStore('crm')
+    console.log(objectStore)
+  }
+
+  function connectDB() {
+    const openDB = window.indexedDB.open("crm", 1);
+
+    openDB.onerror = function () {
+      console.log("Error OPEN DB");
+    };
+
+    openDB.onsuccess = function () {
+      DB = openDB.result;
+    };
   }
 })();
