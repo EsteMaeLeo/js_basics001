1a7e0173568905622b6ebd90704d0325cee4b3a5
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-18 00:10:23 -0500

    new changes

--- projects/crm/js/editclient.js
+++ projects/crm/js/editclient.js
@@ -1,14 +1,23 @@
 (function () {
   let DB;
+  let idClient;
 
-  const nameInput = document.querySelector('#nombre')
+  const nameInput = document.querySelector("#nombre");
+  const emailInput = document.querySelector("#email");
+  const phoneInput = document.querySelector("#telefono");
+  const companyInput = document.querySelector("#empresa");
+
+  form = document.querySelector("#formulario");
 
   document.addEventListener("DOMContentLoaded", () => {
-    connectDB();
+    conectDB();
     //check the id from URL
     const parameterURL = new URLSearchParams(window.location.search);
 
-    const idClient = parameterURL.get("id");
+    //updatef
+    form.addEventListener("submit", updateClient);
+
+    idClient = parameterURL.get("id");
     console.log(idClient);
     if (idClient) {
       setTimeout(() => {
@@ -24,20 +33,19 @@
     const objectStore = transaction.objectStore("crm");
     console.log(objectStore);
     const client = objectStore.openCursor();
-    client.onsuccess = function(e){
+    client.onsuccess = function (e) {
       const cursor = e.target.result;
-      if(cursor){
-
-        if(cursor.value.id === Number(id)){
-          console.log('ID:', cursor.value)
+      if (cursor) {
+        if (cursor.value.id === Number(id)) {
+          console.log("ID:", cursor.value);
           fillForm(cursor.value);
         }
         cursor.continue();
       }
-    }
+    };
   }
 
-  function connectDB() {
+  function conectDB() {
     const openDB = window.indexedDB.open("crm", 1);
 
     openDB.onerror = function () {
@@ -49,9 +57,50 @@
     };
   }
 
-  function fillForm(dataClient){
-    const {name} = dataClient;
+  function updateClient(e) {
+    e.preventDefault();
+    if (
+      nameInput.value === "" ||
+      emailInput.value === "" ||
+      phoneInput.value === "" ||
+      companyInput.value === ""
+    ) {
+      printAlert("Fields are required", "error");
+
+      return;
+    }
+
+    //update client
+    const clientUpdate = {
+      name: nameInput.value,
+      email: emailInput.value,
+      phone: phoneInput.value,
+      company: companyInput.value,
+      id: Number(idClient),
+    };
+    console.log(clientUpdate);
+    const transaction = DB.transaction(["crm"], "readwrite");
+
+    const objectStore = transaction.objectStore("crm");
+    objectStore.put(clientUpdate);
+    transaction.oncomplete = function () {
+      printAlert("Updated Correctly");
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 3000);
+    };
+
+    transaction.onerror = function () {
+      printAlert("Error on update", "error");
+    };
+  }
+
+  function fillForm(dataClient) {
+    const { name, email, phone, company } = dataClient;
 
     nameInput.value = name;
+    phoneInput.value = phone;
+    emailInput.value = email;
+    companyInput.value = company;
   }
 })();
--- projects/crm/js/newclient.js
+++ projects/crm/js/newclient.js
@@ -9,18 +9,6 @@
     form.addEventListener("submit", validClient);
   });
 
-  function conectDB() {
-    const openDB = window.indexedDB.open("crm", 1);
-
-    openDB.onerror = function () {
-      console.log("Error OPEN DB");
-    };
-
-    openDB.onsuccess = function () {
-      DB = openDB.result;
-    };
-  }
-
   function validClient(e) {
     e.preventDefault();
 
@@ -71,40 +59,5 @@
     };
   }
 
-  function printAlert(msg, type) {
-    const alert = document.querySelector(".alert");
 
-    if (!alert) {
-      const divAlert = document.createElement("div");
-      divAlert.classList.add(
-        "px-4",
-        "py-3",
-        "rounded",
-        "max-w-lg",
-        "mx-auto",
-        "mt-6",
-        "text-center",
-        "border",
-        "alert"
-      );
-
-      if (type === "error") {
-        divAlert.classList.add("bg-red-100", "border-red-400", "text-red-700");
-      } else {
-        divAlert.classList.add(
-          "bg-green-100",
-          "border-green-400",
-          "text-green-700"
-        );
-      }
-
-      divAlert.textContent = msg;
-
-      form.appendChild(divAlert);
-
-      setTimeout(() => {
-        divAlert.remove();
-      }, 3000);
-    }
-  }
 })();
