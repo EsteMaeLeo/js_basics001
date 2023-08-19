5eba77f4f159be2d5c5de58251e770f461c83bba
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-19 01:23:33 -0500

    new changes

--- projects/Asyn Promises Callback/app.js
+++ projects/Asyn Promises Callback/app.js
@@ -1,12 +1,21 @@
 //callbacks
-const countries = ['Spain', 'USA', 'Cuba', 'Croatia', 'Egipt','Japan'];
+const countries = ["Spain", "USA", "Cuba", "Croatia", "Egipt", "Japan"];
 
-function showCountries(){
-    setTimeout(() =>{
-        countries.forEach(country =>{
-            console.log(country)
-        })
-    },1000)
+function newCountry(country, callback) {
+  setTimeout(() => {
+    countries.push(country);
+    callback();
+  },2000);
+}
+
+function showCountries() {
+  setTimeout(() => {
+    countries.forEach((country) => {
+      console.log(country);
+    });
+  }, 1000);
 }
 
 showCountries();
+console.log('$$$$')
+newCountry("France", showCountries);
