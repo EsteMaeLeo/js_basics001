88a20afa135d9296470a94d244a883f21b60bc54
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-26 05:45:23 -0500

    new changes

--- projects/Asyn Promises Callback/app3.js
+++ projects/Asyn Promises Callback/app3.js
@@ -9,14 +9,40 @@ const applyDiscount = new Promise((resolve, reject) => {
 });
 
 applyDiscount
-    .then(result =>{
-        console.log(result)
-    })
-    .catch(error =>{
-        console.log(error)
-    })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 //console.log(applyDiscount)
-//fulfilled promise 
-//reject promise 
+//fulfilled promise
+//reject promise
 // pending not fulfilled or reject
+
+const countries = [];
+
+const newCountry = (country) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      countries.push(country);
+      resolve(`Country added ${country}`);
+    }, 2000);
+  });
+
+newCountry("Germany")
+  .then((result) => {
+    console.log(result);
+    console.log(countries);
+    return newCountry("France");
+  })
+  .then((result) => {
+    console.log(result);
+    console.log(countries);
+    return newCountry("England");
+  })
+  .then((result) => {
+    console.log(result);
+    console.log(countries);
+  });
