dc0eebd4fe580b891ff27f94fe0825bdc90bfa51
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-28 05:41:52 -0500

    new changes

--- projects/APIs/app1.js
+++ projects/APIs/app1.js
@@ -1,9 +1,17 @@
 const notifyBtn = document.querySelector("#notify");
+const checkNotify = document.querySelector("#checkNotify");
 
 notifyBtn.addEventListener("click", () => {
-  Notification
-    .requestPermission()
-    .then(result =>{
-        console.log(result);
+  Notification.requestPermission().then((result) => {
+    console.log(result);
+  });
+});
+
+checkNotify.addEventListener("click", () => {
+  if(Notification.permission === "granted") {
+    new Notification("this is the Notification",{
+        icon:'img/twitter.png',
+        body: 'Sending notifications is easy just allowed'
     });
+  }
 });
