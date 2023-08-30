93be9345d16202f1bbfd5a91a2a2fb8cc6d4fde5
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-08-29 23:58:40 -0500

    new changes

--- projects/APIs/app2.js
+++ projects/APIs/app2.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries) => {
     console.log(entries[0]);
   });
+
+  observer.observe(document.querySelector('.premium'))
+
+
 });
