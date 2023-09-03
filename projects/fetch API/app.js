b1857d4085af26c1a4f34934d5905f6f7afaad78
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-02 21:37:58 -0500

    new changes

--- projects/fetch API/app.js
+++ projects/fetch API/app.js
@@ -3,13 +3,20 @@ loadTxtBtn.addEventListener("click", getData);
 
 function getData() {
   const url = "data/data.txt";
-  fetch(url).then((response) => {
-    console.log(response);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.url);
-    console.log(response.type);
-    
-    return response.text();
-  });
+  fetch(url)
+    .then((response) => {
+      console.log(response);
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(response.url);
+      console.log(response.type);
+
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(error =>{
+        console.log(error)
+    });
 }
