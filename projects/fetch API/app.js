3ececd57882f23f3ac653629007c95170dd7e5cc
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-03 21:38:34 -0500

    new changes

--- projects/fetch API/app.js
+++ projects/fetch API/app.js
@@ -1,5 +1,28 @@
 const loadTxtBtn = document.querySelector("#loadTxt");
+const loadJsonBtn = document.querySelector("#loadJSON");
+
 loadTxtBtn.addEventListener("click", getData);
+loadJsonBtn.addEventListener("click", getDataJson);
+
+function getDataJson() {
+  const url = "data/employee.json";
+  fetch(url)
+    .then((response) => {
+      console.log(response);
+      console.log(response.status);
+      console.log(response.statusText);
+      console.log(response.url);
+      console.log(response.type);
+
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 function getData() {
   const url = "data/data.txt";
@@ -16,7 +39,7 @@ function getData() {
     .then((data) => {
       console.log(data);
     })
-    .catch(error =>{
-        console.log(error)
+    .catch((error) => {
+      console.log(error);
     });
 }
