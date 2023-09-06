2cf8efc222c0c5e1349278a72a0b695412889b2e
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-05 22:59:55 -0500

    new changes

--- projects/weather/js/app.js
+++ projects/weather/js/app.js
@@ -2,10 +2,41 @@ const container = document.querySelector(".container");
 const result = document.querySelector("#resultado");
 const form = document.querySelector("#formulario");
 
-window.addEventListener('load', ()=>{
-    form.addEventListener('submit', searchWeather);
-})
+window.addEventListener("load", () => {
+  form.addEventListener("submit", searchWeather);
+});
 
-function searchWeather(e){
-    e.preventDefault();
+function searchWeather(e) {
+  e.preventDefault();
+
+  const city = document.querySelector("#ciudad").value;
+  const country = document.querySelector("#pais").value;
+
+  if (city === "" || country === "") {
+    showError("Both fields are mandatory");
+    return;
+  }
+}
+
+function showError(msg) {
+  console.log(msg);
+
+  const alert = document.createElement("div");
+  alert.classList.add(
+    "bg-red-100",
+    "border-red-400",
+    "text-red-700",
+    "px-4",
+    "py-3",
+    "rounded",
+    "max-w-md",
+    "mx-auto",
+    "mt-6",
+    "text-center"
+  );
+
+  alert.innerHTML = `
+    <strong class="font-bold">Error!</strong>
+    <span class="block">${msg}</span>`;
+  container.appendChild(alert);
 }
