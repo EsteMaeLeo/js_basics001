a825f46ccb1530e9d1451928428cf5fd59cf7a38
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-08 00:12:13 -0500

    adding new example project Recipe API

--- projects/Recipe/js/app.js
+++ projects/Recipe/js/app.js
@@ -1,12 +1,27 @@
+const category = document.querySelector(".form-select");
+
 function initApp() {
   getCategory();
+  console.log(category);
 }
 
 function getCategory() {
   const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => createCategoryList(data))
     .catch((error) => console.log(error));
+}
+
+function createCategoryList(data) {
+  console.log(data);
+  data.categories.forEach((datacat) => {
+    console.log(datacat.strCategory);
+    const option = document.createElement("option");
+    console.log(option);
+    option.text = datacat.strCategory;
+    category.appendChild(option);
+  });
+  
 }
 document.addEventListener("DOMContentLoaded", initApp);
