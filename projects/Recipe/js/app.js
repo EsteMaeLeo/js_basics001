2b6328e41c21bb8a663b159374e8c4261c0d094f
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-10 23:32:37 -0500

    adding new example project Recipe API

--- projects/Recipe/js/app.js
+++ projects/Recipe/js/app.js
@@ -12,19 +12,25 @@ function selectionCategory(e) {
   console.log(selectCategory.value);
   console.log(e.target.value);
   const category = e.target.value;
-  const url =
-    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => getMeals(data))
     .catch((error) => console.log(error));
 }
 
-function getMeals(data){
-    console.log(data)
-    data.meals.forEach(food =>{
-        console.log(food)
-    })
+function getMeals(data) {
+  console.log(data);
+  data.meals.forEach((food) => {
+    //console.log(food)
+    const container = document.createElement("DIV");
+
+    container.classList.add("col-md-4");
+    const card = document.createElement("DIV");
+    card.classList.add("card", "mb-4");
+    container.appendChild(card);
+    console.log(container)
+  });
 }
 
 function getCategory() {
@@ -36,10 +42,10 @@ function getCategory() {
 }
 
 function createCategoryList(data) {
-  console.log(data);
+  //console.log(data);
   data.categories.forEach((datacat) => {
     const { strCategory } = datacat;
-    console.log(datacat.strCategory);
+    //console.log(datacat.strCategory);
     const option = document.createElement("OPTION");
     console.log(option);
     option.value = strCategory;
