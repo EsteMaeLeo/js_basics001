04fcb03bbeae19e4a512cdfacfb254e9f68191eb
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-12 17:54:37 -0500

    adding new example project Recipe API

--- projects/Recipe V2/js/app.js
+++ projects/Recipe V2/js/app.js
@@ -1,6 +1,6 @@
 const divFoods = document.querySelector(".foods");
 
-function apiMeals() {
+ function apiMeals() {
   const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
   fetch(url)
@@ -11,26 +11,40 @@ function apiMeals() {
     });
 }
 
-function createCategoryList(data) {
+ function createCategoryList(data) {
   data.categories.forEach((category) => {
     const { strCategory } = category;
-    const div = document.createElement("DIV");
-    const h2 = document.createElement("H2");
-    h2.textContent = strCategory;
-    divFoods.appendChild(h2);
+
     getFoods(strCategory);
   });
 }
 
-function getFoods(category) {
+async function getFoods(category) {
   const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
-  fetch(url)
+  await fetch(url)
     .then((response) => response.json())
-    .then((data) => displayFoods(data))
+    .then((data) => displayFoods(category, data.meals))
     .catch((error) => console.log(error));
 }
 
-function displayFoods(data) {console.log(data)}
+function displayFoods(category, foodList) {
+
+  console.log(foodList);
+  const div = document.createElement("DIV");
+  const h2 = document.createElement("H2");
+  h2.classList.add("font-bold");
+  h2.textContent = category;
+  divFoods.appendChild(h2)
+
+  foodList.forEach((food) => {
+    const { idMeal, strMeal, strMealThumb } = food;
+    const container = document.createElement("DIV");
+    const heading = document.createElement("H3");
+    heading.textContent = strMeal;
+    container.appendChild(heading);
+    divFoods.appendChild(container);
+  });
+}
 
 function intiApp() {
   apiMeals();
