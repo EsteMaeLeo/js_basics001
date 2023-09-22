0687e218090ab079dd3972004c1eb607444deae8
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-22 05:59:54 -0500

    adding new example project Recipe API

--- projects/image/js/app.js
+++ projects/image/js/app.js
@@ -16,4 +16,31 @@ function valForm(e) {
   }
 }
 
-function showAlert(msg) {}
+function showAlert(msg) {
+  const existAlert = document.querySelector(".bg-red-100");
+  if (!existAlert) {
+    const alert = document.createElement("P");
+    alert.classList.add(
+      "bg-red-100",
+      "border-red-400",
+      "text-red-700",
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-lg",
+      "mx-auto",
+      "mt-6",
+      "text-center"
+    );
+
+    alert.innerHTML = `
+  <strong class="font-bold">Error!</strong>
+  <span class="block sm:inline">${msg}</span> `;
+
+    form.appendChild(alert);
+
+    setTimeout(() => {
+      alert.remove();
+    }, 3000);
+  }
+}
