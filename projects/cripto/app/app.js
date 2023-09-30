b6ca03d25f44cd076a844fa36c245e94c4f5170f
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-30 00:03:38 -0500

    adding new changes

--- projects/cripto/app/app.js
+++ projects/cripto/app/app.js
@@ -1,9 +1,15 @@
-const criptoselect = document.querySelector('#criptomonedas')
+const criptoselect = document.querySelector("#criptomonedas");
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    selectCripto();
-})
+document.addEventListener("DOMContentLoaded", () => {
+  selectCripto();
+});
 
-function selectCripto(){
-    const url = ''
+function selectCripto() {
+  const url =
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
 }
