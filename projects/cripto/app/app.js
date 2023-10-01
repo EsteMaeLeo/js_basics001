3afdd83afa2861ba9f0826b0b8f26d13bc340f84
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-09-30 21:52:54 -0500

    adding new changes

--- projects/cripto/app/app.js
+++ projects/cripto/app/app.js
@@ -1,15 +1,23 @@
 const criptoselect = document.querySelector("#criptomonedas");
 
+//on promise
+
+const obtainCripto = cripto => new Promise(resolve =>{
+    resolve(cripto)
+})
+
 document.addEventListener("DOMContentLoaded", () => {
   selectCripto();
 });
 
 function selectCripto() {
   const url =
-    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
 
   fetch(url)
     .then((response) => response.json())
-    .then((result) => console.log(result))
+    .then((result) => obtainCripto(result.Data))
+    .then(cripto => console.log(cripto))
     .catch((error) => console.log(error));
 }
+
