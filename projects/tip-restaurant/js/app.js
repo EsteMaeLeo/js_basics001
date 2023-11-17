ef86bc9fcd1041577ff2ae6b1ab74ec745a78885
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-11-17 00:05:28 -0600

    new changes

---
+++ projects/tip-restaurant/js/app.js
@@ -0,0 +1,13 @@
+let client = {
+    table: '',
+    time: '',
+    order: []
+}
+
+const btnSaveOrderClient = document.querySelector('#guardar-cliente');
+
+btnSaveOrderClient.addEventListener('click', saveClient)
+
+function saveClient(){
+    
+}
