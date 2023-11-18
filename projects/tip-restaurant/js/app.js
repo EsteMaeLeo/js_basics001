82a44884dad56dc7ac9da92aacfa710a911b1b99
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-11-17 20:44:57 -0600

    new changes

--- projects/tip-restaurant/js/app.js
+++ projects/tip-restaurant/js/app.js
@@ -11,13 +11,27 @@ btnSaveOrderClient.addEventListener("click", saveClient);
 function saveClient() {
   const table = document.querySelector("#mesa").value;
   const time = document.querySelector("#hora").value;
-  console.log(time)
+  console.log(time);
 
   const emptyFields = [table, time].some((field) => field === "");
 
   if (emptyFields) {
-    console.log("Found an empty field");
-  } else {
-    console.log("all fields filled");
+    //console.log("Found an empty field");
+    const existAlert = document.querySelector(".invalid-feedback");
+
+    if (!existAlert) {
+      const alert = document.createElement("DIV");
+
+      alert.classList.add("invalid-feedback", "d-block", "text-center");
+      //alert.textContent = 'All fields are mandatory'
+      alert.textContent = "Todos los campos son mandatorios";
+      document.querySelector(".modal-body form").appendChild(alert);
+
+      //delete alert
+      setTimeout(() => {
+        alert.remove();
+      }, 3000);
+    }
+    return;
   }
 }
