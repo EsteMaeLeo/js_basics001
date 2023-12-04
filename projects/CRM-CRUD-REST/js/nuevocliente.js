48776b83eb9c9272c50eee4d7df12e6690eed9b4
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-04 00:04:52 -0600

    new changes

---
+++ projects/CRM-CRUD-REST/js/nuevocliente.js
@@ -0,0 +1,23 @@
+(function () {
+  const form = document.querySelector("#formulario");
+
+  form.addEventListener("submit", validateClient);
+
+  function validateClient(e) {
+    e.preventDefault();
+
+    const name = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#telefono").value;
+    const company = document.querySelector("#empresa").value;
+
+    const client = {
+      name,
+      email,
+      phone,
+      company,
+    };
+
+    console.log(client);
+  }
+})();
