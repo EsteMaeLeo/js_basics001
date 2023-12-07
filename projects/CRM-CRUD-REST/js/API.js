20bfedb3a6dc1abfaf1318d36dadce511613bc5d
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-07 16:44:29 -0600

    new changes

--- projects/CRM-CRUD-REST/js/API.js
+++ projects/CRM-CRUD-REST/js/API.js
@@ -2,6 +2,19 @@ const url = "http://localhost:4001/clientes";
 
 const newClient = (client) => {
   console.log(client);
+
+  try {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(client),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    window.location.href = "index.html";
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { newClient };
