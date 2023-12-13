4e3916d6c3210779ed9ed421f2010cebf4eebd1c
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-12 23:59:09 -0600

    new changes

--- projects/FunctionalJS/js/02-app.js
+++ projects/FunctionalJS/js/02-app.js
@@ -1,10 +1,8 @@
 const suma2 = (a, b) => a + b;
-const multiply = (a, b) => a + b;
+const multiply = (a, b) => a * b;
 
 const addMultiply = (fn) => fn(10, 20);
 
 console.log(addMultiply(suma2));
 
-const resul2 = suma2;
-
-console.log(result(2, 6));
+console.log(addMultiply(multiply));
