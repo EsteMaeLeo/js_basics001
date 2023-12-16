564d96d052bf362128435d0a21c2a3c9bf30bf3f
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-16 05:42:31 -0600

    new changes

--- projects/FunctionalJS/js/09-app.js
+++ projects/FunctionalJS/js/09-app.js
@@ -3,10 +3,21 @@
 console.log("--PARTIALS AND CURRYING--");
 
 const add = (a, b, c) => a + b + c;
-console.log(add(6, 6, 6));
+console.log(add(4, 5, 6));
 
 const partial = (a) => (b, c) => add(a, b, c);
 
-const firstNumber = partial(5);
-const resultPartial = firstNumber(6, 6);
+const firstNumber = partial(4);
+const resultPartial = firstNumber(5, 6);
 console.log(resultPartial);
+
+const partialTwo = (a) => (b) => (c) => add(a, b, c);
+
+const numberfirst = partialTwo(4);
+const numberSecond = numberfirst(5);
+const resultThird = numberSecond(6);
+
+console.log(resultThird);
+
+const resultPartialFinal = partialTwo(4)(5)(6);
+console.log(resultPartialFinal);
