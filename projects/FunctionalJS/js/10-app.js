db54e63a8effe89aa59575b5d69cc38020c880bc
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-17 05:44:37 -0600

    new changes

--- projects/FunctionalJS/js/10-app.js
+++ projects/FunctionalJS/js/10-app.js
@@ -1,22 +1,40 @@
 //composition
 
+const getName = (info) => ({
+  showName() {
+    console.log(`Name: ${info.name}`);
+  },
+});
 
+const saveEmail = (info) => ({
+  setEmail(email) {
+    console.log(`Saving email in: ${info.name}`);
+    info.email = email;
+  },
+});
 
-function clientComp(name, email, company){
-    let info ={
-        name,
-        email,
-        company
-    }
+function clientComp(name, email, company) {
+  let info = {
+    name,
+    email,
+    company,
+  };
+
+  return Object.assign(info, getName(info), saveEmail(info));
 }
 
-function employeeComp(name, email, job){
-    let info ={
-        name,
-        email,
-        job
-    }
+function employeeComp(name, email, job) {
+  let info = {
+    name,
+    email,
+    job,
+  };
+
+  return Object.assign(info, getName(info), saveEmail(info));
 }
 
-const newClient = clientComp('Jhon Hopkins', 'john@email.com', 'Apple')
-const newEmployee = employeeComp('Peter Potter', 'petter@email.com', 'Manager')
+console.log("--composition--");
+const newClient = clientComp("Jhon Hopkins", null, "Apple");
+newClient.showName();
+const newEmployee = employeeComp("Peter Potter", null, "Manager");
+newEmployee.showName();
