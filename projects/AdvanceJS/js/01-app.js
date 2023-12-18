b6150c16dcb1b5dbcfce7c36a26545be1773a5f6
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-17 22:28:28 -0600

    new changes

--- projects/AdvanceJS/js/01-app.js
+++ projects/AdvanceJS/js/01-app.js
@@ -1,29 +1,44 @@
 //scope
 console.log("---SCOPE---");
 
-const client = "Charles"
+const client = "Charles";
 
-function showClient(){
-    
-    const client = "William"
-    console.log(client)
-
+function showClient() {
+  const client = "William";
+  console.log(client);
 }
 
-console.log(client)
+console.log(client);
 
-showClient()
+showClient();
 
 //hosting
 console.log("---HOSTING---");
 
-showClientFunction('Paul')
-function showClientFunction(name){
-    console.log(`Client name is ${name}`)
+showClientFunction("Paul");
+function showClientFunction(name) {
+  console.log(`Client name is ${name}`);
 }
 
-const showNameExpresion = function (name){
-    console.log(`Client name is ${name}`)
-}
+const showNameExpresion = function (name) {
+  console.log(`Client name is ${name}`);
+};
 
-showNameExpresion('Mike')
+showNameExpresion("Mike");
+
+// coercion refers to the automatic conversion of data types
+console.log("--coercion---");
+
+const number1 = 20;
+const number2 = "20";
+
+console.log(number1 + number2);
+
+console.log(Number(number2));
+
+console.log(number1.toString());
+
+const order = [1, 2, 3, 4];
+
+console.log(order.toString());
+console.log(JSON.stringify(order));
