5b9ed4d09efe13be0dfa3407c93f274fc1efc2f0
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-27 21:48:03 -0600

    new changes

--- projects/ServiceWorkers-PWA/sw.js
+++ projects/ServiceWorkers-PWA/sw.js
@@ -10,6 +10,8 @@ const files = [
   "js/apv.js",
 ];
 
+console.log(files);
+
 //install the service worker
 
 self.addEventListener("install", (e) => {
@@ -17,16 +19,39 @@ self.addEventListener("install", (e) => {
 
   e.waitUntil(
     caches.open(nameCache).then((cache) => {
-      console.log("installing cache");
+      console.log("installing cache ", cache);
       cache.addAll(files);
     })
   );
 });
 
 //activate service worker
+
 self.addEventListener("activate", (e) => {
   console.log("Service Worker activated");
 });
+
+/*
+self.addEventListener("activate", (e) => {
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keyList) =>
+        Promise.all(
+          keyList.map((key) => {
+            if (!nameCache.includes(key)) {
+              return caches.delete(key);
+            }
+          })
+        )
+      )
+      .then(() => {
+        console.log("V2 now ready to handle fetches!");
+      })
+  );
+});*/
 
 //event fetch to download files
 
@@ -41,47 +66,19 @@ console.log(e.request)
       .catch(() => caches.match("error.html"))
   );
 });*/
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'error.html';
+
+const OFFLINE_URL = "error.html";
+
 self.addEventListener("fetch", (e) => {
-  
-      /*e.respondWith(
-        caches
-          .match(e.request)
-          .then((response) => {
-            return response;
-          })
-          .catch((error) => {
-            console.log('Fetch failed; returning offline page instead.', error);
-
-            const cache =  caches.open(CACHE_NAME);
-            const cachedResponse =  cache.match("/error.html");
-            return cachedResponse;
-          })
-      );*/
-      
-    e.respondWith((async () => {
-        try {
-          // First, try to use the navigation preload response if it's supported.
-          const preloadResponse = await e.preloadResponse;
-          if (preloadResponse) {
-            return preloadResponse;
-          }
-          console.log(e.request)
-          const networkResponse = await fetch(e.request);
-          console.log(networkResponse)
-          return networkResponse;
-        } catch (error) {
-          // catch is only triggered if an exception is thrown, which is likely
-          // due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
-          console.log('Fetch failed; returning offline page instead.', error);
-  
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
-        }
-      })());
-    });
-
+  console.log(location.origin);
+  console.log(e.request);
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .catch(() => caches.match("error.html"))
+  );
+});
