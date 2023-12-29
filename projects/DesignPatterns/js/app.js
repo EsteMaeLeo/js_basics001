b2f59918b35e67060a00680d4d93efd0216edd09
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-12-28 23:10:27 -0600

    new changes patterns

--- projects/DesignPatterns/js/app.js
+++ projects/DesignPatterns/js/app.js
@@ -7,5 +7,20 @@ class Person {
   }
 }
 
-const person1 = new Person('John Williams', "email@email.com")
-console.log(person1)
+console.log("CLASS PATTERN");
+
+const person1 = new Person("John Williams", "email@email.com");
+console.log(person1);
+
+//Constructor Pattern
+
+class Client extends Person {
+  constructor(name, email, company) {
+    super(name, email);
+    this.company = company;
+  }
+}
+
+const client = new Client("Mike Knight", "email@email.com", "Google");
+console.log("Constructor Pattern");
+console.log(client);
