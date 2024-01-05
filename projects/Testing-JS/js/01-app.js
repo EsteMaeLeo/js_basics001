b6c7a3bcdbc9a1b5cf0038eea70af4065a2d2963
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-01-05 05:29:15 -0600

    new changes patterns

--- projects/Testing-JS/js/01-app.js
+++ projects/Testing-JS/js/01-app.js
@@ -2,10 +2,23 @@ function add(a, b) {
   return a + b;
 }
 
+function subtraction(a, b) {
+    return a - b;
+  }
+
 let result = add(2, 2);
 let expected = 3;
 
 if(result !== expected){
+    console.error("TEST FAIL: ", result)
+}else{
+    console.log("TEST successfully pass ",result)
+}
+
+ result = subtraction(10, 5);
+ expected = 5;
+
+ if(result !== expected){
     console.error("TEST FAIL: ", result)
 }else{
     console.log("TEST successfully pass ",result)
