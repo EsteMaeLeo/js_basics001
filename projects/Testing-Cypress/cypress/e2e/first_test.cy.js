64c18933d71d10d7f07c5c83591cc40af16bedfd
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-01-10 23:34:21 -0600

    new changes

--- projects/Testing-Cypress/cypress/e2e/first_test.cy.js
+++ projects/Testing-Cypress/cypress/e2e/first_test.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+//<reference types="cypress"/>
+
+describe("template spec", () => {
+  it("Main load page", () => {
+    cy.visit("http://127.0.0.1:5501/projects/Testing-Cypress/index.html");
+    cy.contains("h1", "Administrador de Pacientes de Veterinaria");
+  });
+});
