9d4c0f7339b4539784611b3f7ab35157c26edd53
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-01-12 05:51:11 -0600

    new changes

--- projects/Testing-Cypress/cypress/e2e/create_app.cy.js
+++ projects/Testing-Cypress/cypress/e2e/create_app.cy.js
@@ -5,5 +5,9 @@ describe("validate form", () => {
 
     cy.get('[data-cy="input-pet"]').type("Dogger");
     cy.get('[data-cy="input-owner"]').type("Joan Fez");
+    cy.get('[data-cy="input-phone"]').type("154515099");
+    cy.get('[data-cy="input-date"]').type("2024-01-22");
+    cy.get('[data-cy="input-hour"]').type("12:15");
+    cy.get('[data-cy="input-symptoms"]').type("Very sick and sad");
   });
 });
