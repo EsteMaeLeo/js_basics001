b31d455acecce4013a444fd0e21907fc9d121486
EsteMaeLeo <leonel_morales20@hotmail.com>
2024-02-01 23:03:45 -0600

    new changes

--- projects/agencyNode/controllers/dist/reviewsController.dev.js
+++ projects/agencyNode/controllers/dist/reviewsController.dev.js
@@ -6,7 +6,32 @@ Object.defineProperty(exports, "__esModule", {
 exports.saveReviews = void 0;
 
 var saveReviews = function saveReviews(req, res) {
-  console.log(req.body);
+  //validate
+  var _req$body = req.body,
+      name = _req$body.name,
+      email = _req$body.email,
+      message = _req$body.message;
+  var errorLog = [];
+
+  if (name.trim() === "") {
+    errorLog.push({
+      message: "Name is empty"
+    });
+  }
+
+  if (email.trim() === "") {
+    errorLog.push({
+      message: "email is empty"
+    });
+  }
+
+  if (message.trim() === "") {
+    errorLog.push({
+      message: "message is empty"
+    });
+  }
+
+  console.log(errorLog);
 };
 
 exports.saveReviews = saveReviews;
--- projects/agencyNode/controllers/reviewsController.js
+++ projects/agencyNode/controllers/reviewsController.js
@@ -1,5 +1,18 @@
 const saveReviews = (req, res) => {
-  console.log(req.body);
+  //validate
+  const { name, email, message } = req.body;
+
+  const errorLog = [];
+  if (name.trim() === "") {
+    errorLog.push({ message: "Name is empty" });
+  }
+  if (email.trim() === "") {
+    errorLog.push({ message: "email is empty" });
+  }
+  if (message.trim() === "") {
+    errorLog.push({ message: "message is empty" });
+  }
+  console.log(errorLog);
 };
 
 export { saveReviews };
